893e75601dc0d5b5d339922711fc96c60f8c2bdf
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2015-08-12 12:56:10 +0200

    define START/END spec file to use crtbegin/crtend

--- gcc/config/hermit.h
+++ gcc/config/hermit.h
@@ -14,6 +14,9 @@
 /* Don't automatically add extern "C" { } around header files. */
 #undef  NO_IMPLICIT_EXTERN_C
 #define NO_IMPLICIT_EXTERN_C 1
+
+/* Switch into a generic section.  */
+#define TARGET_ASM_NAMED_SECTION  default_elf_asm_named_section
  
 /* Additional predefined macros. */
 #undef TARGET_OS_CPP_BUILTINS
@@ -25,3 +28,9 @@
     builtin_assert ("system=unix");   \
     builtin_assert ("system=posix");   \
   } while(0);
+
+#undef  STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+#undef  ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
