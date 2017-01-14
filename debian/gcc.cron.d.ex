c85965325cae3d4050be5ccb846caf73234124eb
Daniel Krebs <github@daniel-krebs.net>
2017-01-14 23:50:15 +0100

    deb: remove unused files

--- debian/gcc.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gcc package
-#
-0 4	* * *	root	[ -x /usr/bin/gcc_maintenance ] && /usr/bin/gcc_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gcc):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="gcc" command="/usr/bin/gcc"
